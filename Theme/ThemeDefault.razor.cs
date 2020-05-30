9c8eec4866f2ae89e5dbb5a724179aafcd717ac5
ijungleboy <daniel.mettler@2sic.com>
2020-05-30 09:05:56 +0200

    minor

---
+++ Theme/ThemeDefault.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Oqtane.Models;
+using Oqtane.Shared;
+
+namespace Example.Oqt.Themes.Template
+{
+    /// <summary>
+    /// This is the Code-Behind example for the Theme-Default
+    /// Such simple scenarios wouldn't really need this, because tho code could just be
+    /// in the @code section of the normal .razor file.
+    /// </summary>
+    public partial class ThemeDefault
+    {
+        /// <summary>
+        /// Provide a list of resources (js/css)
+        /// </summary>
+        public override List<Resource> Resources => new List<Resource>
+        {
+            // JavaScript Resources
+            new Resource { ResourceType = ResourceType.Script, Url = ThemePath() + "dist/index.js" },
+
+            // CSS resources
+            new Resource { ResourceType = ResourceType.Stylesheet, Url = "https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css",
+                // , Integrity = "sha384-l7xaoY0cJM4h9xh1RfazbgJVUZvdtyLWPueWNtLAphf/UbBgOVzqbOTogxPwYLHM", 
+                CrossOrigin = "anonymous" },
+            new Resource { ResourceType = ResourceType.Stylesheet, Url = ThemePath() + "dist/theme.css" }
+        };
+    }
+}
