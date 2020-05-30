8be7c7fe88afa1b256698cd5048b19650a1d1efd
ijungleboy <daniel.mettler@2sic.com>
2020-05-30 11:29:51 +0200

    fix nuget generation - path was missing dist

--- Theme/dist/theme.js
+++ Theme/dist/theme.js
@@ -1,11 +1,14 @@
 // This is the starting point for scripts of this theme
 // As of now, it just imports a dummy message
+/**
+ * Demo class
+ */
 class Example {
     hello(name) {
         console.log(`Hello from ${name}`);
     }
 }
+// Create an instance, run the code
 const ex = new Example();
 ex.hello("Template Oqtane Theme");
-console.log('Hello old from the Template Oqtane Theme');
 //# sourceMappingURL=theme.js.map
