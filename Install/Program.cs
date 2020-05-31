986631af9c7308c23480e87538a2dcaed6ffd05d
ijungleboy <daniel.mettler@2sic.com>
2020-05-31 10:30:05 +0200

    Enable F5 building (instead of ctrl+shift+alt+b)

---
+++ Install/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Install
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Completed building the Template Theme for Oqtane!");
+            Console.WriteLine("Will now copy-deploy to target");
+
+
+            //Console.WriteLine("Executing path: " + Environment.CurrentDirectory);
+            //Go from build folder to the project folder
+            System.IO.Directory.SetCurrentDirectory("../../../");
+            Console.WriteLine("Executing path: " + Environment.CurrentDirectory);
+
+            Process.Start("build-helpers\\debug.cmd");
+
+            //var cmd = new Process();
+            //cmd.StartInfo.FileName = ""
+        }
+    }
+}
